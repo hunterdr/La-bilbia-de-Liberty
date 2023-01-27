cc356bd24f5e907aa33efb483e5ec954f28200fa
Marlon Vasquez <vmarlon007@gmail.com>
2023-01-27 02:05:01 -0400

    add test

--- documentos/models.py
+++ documentos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Documentos(models.Model):
+    nombre = models.CharField(max_length=255, blank=False, null=False)
+    fecha_subida = models.DateField(auto_now=True)
+    descripcion = models.TextField(null=False, blank=False)
+    documento = models.ImageField(upload_to='foto/', max_length=255, null=False, blank=False)
+
+
+    def __str__(self):
+        return self.nombre
+
